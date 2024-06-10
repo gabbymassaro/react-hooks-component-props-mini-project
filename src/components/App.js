a854cf79371fe1d8e706af707a2d480994962132
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-10 10:54:36 -0400

    Remove console log

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import Header from "./Header"
 import About from "./About"
 import blogData from "../data/blog";
 
-// console.log(blogData);
-
 function App() {
   return (
     <div className="App">
