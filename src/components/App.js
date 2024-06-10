28e26e758cf8ffeefb05cab4284d81dbc48e1eff
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-10 13:19:02 -0400

    Fix tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header"
 import About from "./About"
+import ArticleList from "./ArticleList"
 import blogData from "../data/blog";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header name={blogData.name}/>
       <About image={blogData.image} about={blogData.about}/>
+      <ArticleList posts={blogData.posts}/>
     </div>
   );
 }
